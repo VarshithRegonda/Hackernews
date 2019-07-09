9614f6884106f0617455dd95c57237f83c46c942
VarshithRegonda <varshith4869@gmail.com>
2019-07-09 19:37:38 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const app = require('express')();
 const passport = require('passport');
-const bodyParser= require('body-parser');
+const ejs = require('ejs')
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
-const user =require('./routes/user')
+const users = require('./routes/user')
+const profile = require('./routes/profile')
+var blogs = require('./routes/blog')
+const jwt = require("jsonwebtoken")
 const db = require('./config/keys').uri;
-mongoose.connect(db,{useNewUrlParser:true})
-.then(()=>console.log('mongodb is connected..'))
-.catch(err=>console.log(err));
+const Blogs = require('./models/blog')
+mongoose.connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log('mongodb is connected..'))
+    .catch(err => console.log(err));
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use('/user',user)
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.set('view engine', 'ejs')
+app.use('/user', users)
+app.use('/profile', profile);
+app.use('/blog', blogs)
+app.use(passport.initialize());
+app.get('/', function (req, res) {
+    res.render('home', {
+        welcomeMesssage: "This message was rendered on the server",
+        headerTitle: "HACKER NEWS HOME PAGE"
+    })
+    app.get('/login', function (req, res) {
+        res.render('login', {
+            message: "Enter the credentials"
+        })
+        
+        })
+        app.get('/submit/', function (req, res) {
+            res.render('addposts', {
+                message: "Enter the credentials"
+            })
+    })
+  
+    
+        })
+    
 const PORT = process.env.port || 5000;
-app.listen(PORT,()=>console.log('sever listening on port' +PORT))
+app.listen(PORT, () => console.log('sever listening on port' + PORT));
